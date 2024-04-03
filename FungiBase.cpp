10c057e123b03f267082f722540cf97adfe2c884
Jacob Costen <32058792+oculometric@users.noreply.github.com>
2024-04-04 00:55:43 +0100

    added raygui and raylib

--- FungiBase.cpp
+++ FungiBase.cpp
@@ -7,6 +7,9 @@
 #include "serialise.h"
 #include "database.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 int main()
 {
     cout << "Hello World!\n";
@@ -14,4 +17,26 @@ int main()
     FBDatabase database("database.json");
     cout << database.reload() << endl;
     cout << database.flush() << endl;
+
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(400, 400, "test");
+    GuiLoadStyle("../style_lavanda.rgs");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(GetColor(0x5b5b81ff /*GuiGetStyle(DEFAULT, BACKGROUND_COLOR)*/));
+        if (GuiButton(Rectangle{ 10, 10, 100, 100 }, "#30#button"))
+            cout << "button!" << endl;
+
+        if (IsWindowResized())
+            cout << "resize!" << endl;
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
